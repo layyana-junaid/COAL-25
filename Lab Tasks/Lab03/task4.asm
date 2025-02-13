19fc55c50c45c1d6ec9b37fe4c00141dfc6c6b31
Layyana Junaid <142867946+layyana-junaid@users.noreply.github.com>
2025-02-13 14:50:34 +0500

    Create task4.asm

---
+++ Lab Tasks/Lab03/task4.asm
@@ -0,0 +1,17 @@
+; Layyana Junaid 23k-0056 BSAI-4A
+; Lab 3 Task 4
+
+INCLUDE Irvine32.inc
+
+.data
+   colorName BYTE "Purple", 0  
+   A WORD 12
+   B WORD 2
+   C WORD 13
+   D WORD 8
+   E WORD 14
+
+
+
+
+
