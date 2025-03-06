041f3cc2ed4c5970d51455e18ce78e4234aa43ce
Layyana Junaid <142867946+layyana-junaid@users.noreply.github.com>
2025-03-06 12:56:01 +0500

    Create task3.asm

---
+++ Lab Tasks/Lab05/task3.asm
@@ -0,0 +1,40 @@
+TITLE task
+include irvine32.inc
+
+.data
+	arr BYTE 61,43,11,52,25
+	newarr BYTE LENGTHOF arr DUP(?)
+
+.code
+main PROC
+    mov esi, OFFSET arr
+
+    mov al, [esi+2] 
+    mov newarr[0], al
+
+    mov al, [esi+4] 
+    mov newarr[1], al
+
+    mov al, [esi+1]  
+    mov newarr[2], al
+
+    mov al, [esi+3]  
+    mov newarr[3], al
+
+    mov al, [esi] 
+    mov newarr[4], al
+
+    mov esi, 0
+    mov ecx, LENGTHOF newarr
+
+printarr:
+    movzx eax, newarr[esi*TYPE newarr]
+    call WriteInt
+    call Crlf ; for leaving lines for clean output
+    inc esi
+    loop printarr
+; The above code is already generalised to account for separate data types therefore the only change needed for 
+; WORD or DWORD operation is changing al to ax and eax respectively
+    exit
+main endp
+end main
