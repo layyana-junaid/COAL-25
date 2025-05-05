1e52323c235a731ad48c773471b994b704bb2571
Layyana Junaid <142867946+layyana-junaid@users.noreply.github.com>
2025-05-05 21:33:17 +0500

    Create task1.asm

---
+++ Lab Tasks/Lab10/task1.asm
@@ -0,0 +1,35 @@
+include Irvine32.inc
+
+.data
+num1 dword 5
+num2 dword 7
+num3 dword 3
+
+.code
+main proc
+    ; Push parameters onto stack in reverse order
+    push num3
+    push num2
+    push num1
+    call ThreeProd
+    add esp, 12       ; Clean up stack
+    
+    exit
+main endp
+
+ThreeProd proc
+    push ebp
+    mov ebp, esp
+    
+    mov eax, [ebp+8]   ; num1
+    imul eax, [ebp+12] ; num1 * num2
+    imul eax, [ebp+16] ; (num1 * num2) * num3
+    
+    call WriteInt      ; Display product
+    call Crlf
+    
+    pop ebp
+    ret
+ThreeProd endp
+
+end main
